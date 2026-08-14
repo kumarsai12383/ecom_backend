c2ca6ea531edc4adb6af43972a1d227191ace55f
kumarsai12383 <kumarsai05072005@gmail.com>
2026-08-14 22:34:49 +0530

    port updated

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ mongoose
     console.log(error);
   });
 app.use("/api/products", productRoutes);
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
